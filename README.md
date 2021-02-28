f71f7f7f08484c454d4a7e820918e03cc9950ec6
Marco Wegner <devel@marcowegner.eu>
2021-02-28 15:34:11 +0100

    Initial express setup
    - added config setup
    - added express setup with routing-controllers
    - added database connection (mariadb, typeorm)

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ## Package versions
 
+- `class-transformer` set to exact version 0.3.1 for `routing-controllers`
+- `class-validator` set to exact version 0.12.2 for `routing-controllers`
 - `typescript` restricted to < 4.2.0 due to the fact that `eslint` comlains otherwise
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello, world!");
+import "reflect-metadata";
+
+import config from "config";
+import express from "express";
+
+import { useExpressServer } from "routing-controllers";
+import { createConnection } from "typeorm";
+
+const connectionName = config.get("ormconfig.connection") as string;
+createConnection(connectionName)
+    .then(() => {
+        const app = express();
+
+        useExpressServer(app, {
+            routePrefix: "/api",
+            classTransformer: true,
+            validation: true
+        });
+
+        const port = config.get("express.port") as number;
+        app.listen(port, "0.0.0.0", () => {
+            console.log(`Server listening on port ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
