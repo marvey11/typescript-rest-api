45e440fbf7f93ba35d818f9f12cf6f5d8cabf770
Marco Wegner <devel@marcowegner.eu>
2021-02-28 15:42:45 +0100

    Adding cors and body-parser
    - also added preliminary (empty) controllers list

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 
+import bodyParser from "body-parser";
 import config from "config";
+import cors from "cors";
 import express from "express";
 
 import { useExpressServer } from "routing-controllers";
@@ -10,11 +12,14 @@ const connectionName = config.get("ormconfig.connection") as string;
 createConnection(connectionName)
     .then(() => {
         const app = express();
+        app.use(cors());
+        app.use(bodyParser.json());
 
         useExpressServer(app, {
             routePrefix: "/api",
             classTransformer: true,
-            validation: true
+            validation: true,
+            controllers: []
         });
 
         const port = config.get("express.port") as number;
